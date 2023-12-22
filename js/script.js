222a1aaa95cae38ddecf7201d5f1cdbf3b3f11b7
Moeen Ahmad <moeenranjha01@gmail.com>
2023-12-22 17:16:51 +0000

    Updated 1 file via CloudCannon.

--- js/script.js
+++ js/script.js
@@ -212,6 +212,6 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
 function onSubmit(token) {
   console.log('called')
-  window.location.href = '/thankyou';
+  window.location.href = 'thankyou.html';
   document.getElementById("contact-form").submit();
 }
